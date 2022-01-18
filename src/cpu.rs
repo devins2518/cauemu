a1029b71a30098e898819cd8d7b8983cd93d4e32
Devin Singh <drsingh2518@icloud.com>
2022-01-18 07:03:39 -0600

    improve argument api

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,4 @@
-use std::ptr::NonNull;
+use std::{ops::Deref, ptr::NonNull};
 
 pub struct Arm7TDMI {
     registers: [u32; 16],
@@ -121,12 +121,78 @@ enum Mode {
     Undefined,
 }
 
-enum Argument<'a> {
-    Register(&'a u32),
-    Expr(u8),
+enum Operand<'a> {
+    Immediate(u32),
+    LogShiftLImm(u32, u8),
+    LogShiftRImm(u32, u8),
+    RotateRImm(u32, u8),
+    Register(&'a mut u32),
+    LogShiftLReg(u32, u32),
+    LogShiftRReg(u32, u32),
+    ArithShiftR(u32, u32),
+    RotateRReg(u32, u32),
+    RotateRExt(u32),
+}
+
+impl<'a> Operand<'a> {
+    fn get(self, cpu: &mut Arm7TDMI) -> u32 {
+        match self {
+            Operand::Immediate(v) => v,
+            Operand::LogShiftLImm(v, s) => v << s,
+            Operand::LogShiftRImm(v, s) => v >> s,
+            Operand::RotateRImm(v, s) => v.rotate_right(s as u32),
+            Operand::Register(r) => *r,
+            Operand::LogShiftLReg(v, r) => v << r,
+            Operand::LogShiftRReg(v, r) => v >> r,
+            Operand::ArithShiftR(v, r) => (v as i32 >> r) as u32,
+            Operand::RotateRReg(v, r) => v.rotate_right(r),
+            Operand::RotateRExt(r) => {
+                let c = unsafe { &mut cpu.cpsr.as_mut().carry };
+                let one = r & 0x1;
+                let r = ((*c as u32) << 31) | (r >> 1);
+                *c = one == 1;
+                r
+            }
+        }
+    }
 }
 
 // Instruction set implementation
 /// The ARM7TDMI implements the ARMv4T instruction set. Notable features are the 16 bit Thumb
 /// instruction set, three stage pipeline, and the lack of a 26 bit addressing mode.
-impl Arm7TDMI {}
+impl Arm7TDMI {
+    fn mov(&mut self, rd: Operand, op2: Operand) {
+        if let Operand::Register(r) = rd {
+            let val = op2.get(self);
+            *r = val;
+        } else {
+            unsafe { std::hint::unreachable_unchecked() }
+        }
+    }
+    fn mvn(&mut self, rd: Operand, op2: Operand) {
+        unimplemented!()
+    }
+    fn mrs(&mut self, rd: Operand, op2: Operand) {
+        unimplemented!()
+    }
+    fn msr(&mut self, rd: Operand, op2: Operand) {
+        unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_operands() {
+        let op = Operand::RotateRExt(0b10100101100011010011010001101001);
+        let mut cpu = Arm7TDMI::new();
+        cpu.init();
+        unsafe {
+            cpu.cpsr.as_mut().carry = false;
+        }
+        assert_eq!(op.get(&mut cpu), 0b01010010110001101001101000110100);
+        assert!(unsafe { cpu.cpsr.as_ref().carry });
+    }
+}
